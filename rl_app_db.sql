5339725d5cc211b91c88c7922958a66bdfd46e5d
riyanadityapradana <riyanadityapradanaa@gmail.com>
2025-11-07 11:00:05 +0800

    menambahkan sql rl_app_db

---
+++ rl_app_db.sql
@@ -0,0 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 07, 2025 at 03:58 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rl_app_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_attempts`
+--
+
+CREATE TABLE `login_attempts` (
+  `id` int(11) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `attempt_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login_attempts`
+--
+
+INSERT INTO `login_attempts` (`id`, `ip_address`, `username`, `attempt_time`) VALUES
+(7, '::1', 'admin', '2025-10-10 02:10:03'),
+(9, '::1', 'gizi', '2025-10-10 06:56:36'),
+(10, '192.168.1.111', 'gizi', '2025-10-11 02:45:13'),
+(11, '::1', 'riyan', '2025-10-28 00:53:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama_lengkap` varchar(100) DEFAULT NULL,
+  `role` enum('Admin','Pemasaran','Kepegawaian','Perawat','Gizi','User','RM') DEFAULT 'User',
+  `status` enum('Aktif','Nonaktif') DEFAULT 'Aktif',
+  `last_login` datetime DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `nama_lengkap`, `role`, `status`, `last_login`, `created_at`) VALUES
+(1, 'riyan', '210896', 'Riyan Aditya Pradana, S.Kom', 'Admin', 'Aktif', '2025-11-03 14:33:51', '2025-10-10 02:19:51'),
+(2, 'admin', 'admin123', 'Administrator', 'Admin', 'Aktif', '2025-10-18 12:14:21', '2025-10-10 00:53:58'),
+(5, 'gizi', 'unitgizi25', 'Unit Gizi', 'Gizi', 'Aktif', '2025-10-14 11:02:29', '2025-10-10 02:53:37'),
+(6, 'rekammedik', 'unitrm25', 'purwanto', 'RM', 'Aktif', NULL, '2025-10-10 13:09:33'),
+(7, 'arien', 'admin123', 'Qhusnul Arinda, Amd. Far', 'Admin', 'Aktif', NULL, '2025-10-11 03:56:23'),
+(8, '-1', 'admin123', 'Ika Aprillia, S.Kom', 'Admin', 'Aktif', NULL, '2025-10-11 03:57:47'),
+(9, 'adi', '12345', 'Ahmad Noor Fuadi', 'Pemasaran', 'Aktif', NULL, '2025-10-15 05:55:46');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_remember_tokens`
+--
+
+CREATE TABLE `user_remember_tokens` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `expires_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_ip_username` (`ip_address`,`username`),
+  ADD KEY `idx_attempt_time` (`attempt_time`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `username_2` (`username`);
+
+--
+-- Indexes for table `user_remember_tokens`
+--
+ALTER TABLE `user_remember_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `token` (`token`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `user_remember_tokens`
+--
+ALTER TABLE `user_remember_tokens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_remember_tokens`
+--
+ALTER TABLE `user_remember_tokens`
+  ADD CONSTRAINT `user_remember_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
